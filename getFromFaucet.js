a1659bdac01cb447ba22c516131328199eeb237d
Pranav <pranavlakkadi@gmail.com>
2024-02-11 18:12:53 +0530

    changes

--- getFromFaucet.js
+++ getFromFaucet.js
@@ -1,5 +1,5 @@
 const { ethers } = require('hardhat');
-const { fhenixjs } = require("fhenixjs");
+const { getFunds,fhenixjs } = require("fhenix-hardhat-plugin");
 const axios = require('axios');
 require('dotenv').config();
 
@@ -7,6 +7,7 @@ async function mnemonicToAddress() {
     let words = process.env.MNEMONIC;
 
     // const mnemonic = ethers.Mnemonic.fromPhrase(words);
+    // const mnemonic = ethers.Wallet.fromMnemonic(words)
     // if (!mnemonic) {
     //     throw new Error("No MNEMONIC in .env file")
     // }
@@ -15,15 +16,15 @@ async function mnemonicToAddress() {
 
     console.log("Ethereum address: " + wallet.address);
 
-    await fhenixjs.getFunds(wallet.address);
+    // await fhenixjs.getFunds(wallet.address);
     return wallet.address;
     // let x = 0x0319069ca5e383e40d6d19fb3e76e8b22b91cf4b0df661850c8e1de01624ab1d10
     // return x
 }
 
 async function callFaucet(address) {
-    const response = await axios.get(`http://localhost:42000/faucet?address=${address}`);
-    await fhenixjs.getFunds("your_wallet_address");
+    const response = await axios.get(`http://localhost:42000/faucet?address=${"0xd7702EB6Ca4C101C918f7d4eaBeDc36e36260482"}`);
+    // await fhenixjs.getFunds(address);
     const data = await response.data;
     console.log(`Success!: ${JSON.stringify(data)}`);
 }
